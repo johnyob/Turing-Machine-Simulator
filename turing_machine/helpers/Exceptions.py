61f3caf9e6f70986eb01f039b02dca2b6ff6b136
johnyob <johnyob132@gmail.com>
2018-08-29 19:05:13 +0100

    Create Exceptions.py

---
+++ turing_machine/helpers/Exceptions.py
@@ -0,0 +1,18 @@
+class TuringMachineDataTypeException(Exception):
+    pass
+
+
+class TuringMachineTransitionFunctionException(Exception):
+    pass
+
+
+class TuringMachineStateException(Exception):
+    pass
+
+
+class TuringMachineSymbolException(Exception):
+    pass
+
+
+class TuringMachineMovementException(Exception):
+    pass
